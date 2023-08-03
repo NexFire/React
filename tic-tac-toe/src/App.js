b44ea64065502d595c0bd40bccc481b64cea39dc
nit_o <max.tyxl@gmail.com>
2023-08-03 21:13:09 +0200

    Progress on tic-tac-toe

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -3,30 +3,56 @@ import { useState } from 'react';
 import Select from 'react-select';
 import Button from '@mui/material/Button';  
 
-function Square(){
-  return (
-    <div
-    class="square"
-    >
-      this is your square
-    </div>
-  );
-}
-
 function App() {
 
+
+  function Square(props){
+    function defaultOnPress(){
+      console.log("This is "+props.id+" square");
+    }
+    console.log("Id: "+props.id);
+    console.log("Row: "+Math.floor(props.id/props.size)+" lol");
+    console.log("Column"+props.id%props.size);
+    var icon={
+      0:"",
+      1:"X",
+      2:"O",
+    };
+    //style={{width:`${Math.floor(50/props.size)}vw`,height:`${Math.floor(50/props.size)}vw`,gridRow:(Math.floor(props.id/props.size))+1,gridColumn:(props.id%props.size)+1}}
+    //{icon[props.onMove]}  `${Math.floor(50/props.size)}vw`
+    console.log(`${Math.floor(50/props.size)}vw`);
+    console.log("id:"+props.id+" row:"+((Math.floor(props.id/props.size))+1)+" column: "+((props.id%props.size)+1));
+    return (
+      <div
+      className="squarer"
+      onClick={defaultOnPress}
+      style={{}}
+      >
+        <div className='icon'>
+        x
+        </div>
+      </div>
+    );
+  }
+
+
   function PickAGame(setSelected){
-    const options = [
-      { value: 1, label: 1 },
-      { value: 2, label: 2 },
-      { value: 3, label: 3 },
-      { value: 4, label: 4 },
-      { value: 5, label: 5 },
-      { value: 6, label: 6 },
-    ]
+    const options = [];
+    for(var a=3;a<=15;a++){
+      options.push({value:a,label:a});
+    }
+    const playModeOptions = [
+      { value: 1, label: "Local" },
+      { value: 2, label: "Easy (AI)" },
+      { value: 3, label: "Medium (AI)" },
+      { value: 4, label: "Hard (AI)" },
+      { value: 5, label: "Ultra Hard (AI)" },
+      { value: 6, label: "Shit Your Pants (AI)" },
+    ];
     return(
       <div>
         <Select options={options} onChange={handleChange} autoFocus={true}/>
+        <Select options={playModeOptions}/>
         <Button variant="outlined" onClick={()=>setNumberPicked(true)}>Outlined</Button>
       </div>
     );
@@ -40,10 +66,21 @@ function App() {
   function Game(){
     const list=[];
     for (var a=0;a<selected*selected;a++){
-      list.push(Square);
+      list.push(<Square key={a} id={a} onMove={onMove} size={selected}/>);
     }
+    var gridTemplate="";
+    for (var a=0;a<selected;a++){
+      gridTemplate+=` ${Math.floor(50/selected)}vw`;
+    }
+    console.log("Our template: "+gridTemplate+" lol");
     return(
-    <div className="grid-container">
+      <div className="game-main-container">
+        <div>
+          Now moves: 
+        </div>
+        <div className="grid-container" style={{}} >
+          {list}
+        </div>
         </div>
     );
   }
@@ -57,6 +94,8 @@ function App() {
   const [bgColor,setBgColor]=useState([0,0,0]);
   const [numberPicked,setNumberPicked]=useState(false);
   const [selected, setSelected] = useState(1);
+  const [onMove,setMove]=useState(0);
+  console.log("This is on Move: "+onMove+" lol");
   return (
     <div className="main-container" style={{backgroundColor:`rgb(${bgColor[0]},${bgColor[1]},${bgColor[2]})`}}
     >
